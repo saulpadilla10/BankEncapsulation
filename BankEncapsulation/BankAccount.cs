e4650739702ec502c7111d206ff124612d28d660
saulpadilla10 <saulp1021@yahoo.com>
2023-06-04 09:04:04 -0700

    Banking

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+
+		private double balance;
+
+		public void Deposit(double depositAmount)
+		{
+
+			balance += depositAmount;
+		}
+		
+		public double GetBalance()
+		{
+			return balance;
+		}
+		
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace BankEncapsulation
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account1 = new BankAccount();
+
+            double userInput;
+            bool deposit;
+
+            do
+            {
+                Console.WriteLine("Deposit Amout:");
+
+                deposit = double.TryParse(Console.ReadLine(), out userInput);
+                account1.Deposit(userInput);
+
+
+            } while (deposit == false);
+
+
+            Console.WriteLine("To check balance, enter Y or press Enter to stop");
+            string yesno = Console.ReadLine();
+
+            if (yesno.ToUpper() == "Y")
+            {
+                Console.WriteLine($"Balance: {account1.GetBalance()}");
+            }
+            else if (yesno == null)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Goodbye!");
+            }
         }
     }
 }
